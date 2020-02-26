e37afc9c63817b33497cf61dc02c2e650753af98
kohei-iwase <me.tekelily@gmail.com>
2020-02-26 16:10:19 +0000

    rspec導入

--- test/controllers/sesions_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class SesionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get sesions_new_url
-    assert_response :success
-  end
-
-end
